95d3028b7825e25d8991a7bcd6e8f83d54d3e1b0
Yaroslav Zozulia <zyvpeople@gmail.com>
2016-06-04 00:09:38 +0300

    update progress every 1 second

--- app/src/main/java/com/develop/zuzik/audioplayerexample/player/player_states/StartedPlayerState.java
+++ app/src/main/java/com/develop/zuzik/audioplayerexample/player/player_states/StartedPlayerState.java
@@ -6,11 +6,22 @@ import com.develop.zuzik.audioplayerexample.player.PlaybackState;
 import com.develop.zuzik.audioplayerexample.player.PlayerStateContainer;
 import com.develop.zuzik.audioplayerexample.player.player_initializer.PlayerInitializer;
 
+import java.util.concurrent.TimeUnit;
+
+import rx.Observable;
+import rx.Subscription;
+import rx.android.schedulers.AndroidSchedulers;
+
 /**
  * User: zuzik
  * Date: 5/29/16
  */
 public class StartedPlayerState extends BasePlayerState {
+
+	private final Observable<Long> playerProgressObservable = Observable
+			.interval(1, TimeUnit.SECONDS)
+			.observeOn(AndroidSchedulers.mainThread());
+	private Subscription playerProgressSubscription;
 
 	public StartedPlayerState(MediaPlayer player, PlayerInitializer initializer, PlayerStateContainer stateContainer) {
 		super(player, initializer, stateContainer);
@@ -20,11 +31,14 @@ public class StartedPlayerState extends BasePlayerState {
 	public void set() {
 		super.set();
 		onPlaybackStateChanged(PlaybackState.PLAYING);
+		this.playerProgressSubscription = this.playerProgressObservable.subscribe(aLong ->
+				onPlaybackStateChanged(PlaybackState.PLAYING));
 	}
 
 	@Override
 	public void pause() {
 		super.pause();
+		this.playerProgressSubscription.unsubscribe();
 		getPlayer().pause();
 		setState(new PausedPlayerState(getPlayer(), getInitializer(), getStateContainer()));
 	}
