679e482c0d69bfe12dac4eb89cf78fd08a86f2c1
Yaroslav Zozulia <zyvpeople@gmail.com>
2016-07-04 23:08:27 +0300

    add logic for add and remove fragment

--- app/src/main/java/com/develop/zuzik/audioplayerexample/presentation/activities/ExampleActivity.java
+++ app/src/main/java/com/develop/zuzik/audioplayerexample/presentation/activities/ExampleActivity.java
@@ -1,6 +1,7 @@
 package com.develop.zuzik.audioplayerexample.presentation.activities;
 
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 
 import com.develop.zuzik.audioplayerexample.R;
@@ -19,6 +20,47 @@ public class ExampleActivity extends AppCompatActivity implements ExampleFragmen
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_example);
+
+		findViewById(R.id.addLeft).setOnClickListener(v -> {
+			Fragment fragment = getSupportFragmentManager().findFragmentById(R.id.leftContainer);
+			if (fragment == null) {
+				fragment = new ExampleFragment();
+				getSupportFragmentManager()
+						.beginTransaction()
+						.add(R.id.leftContainer, fragment)
+						.commit();
+			}
+		});
+		findViewById(R.id.removeLeft).setOnClickListener(v -> {
+			Fragment fragment = getSupportFragmentManager().findFragmentById(R.id.leftContainer);
+			if (fragment != null) {
+				getSupportFragmentManager()
+						.beginTransaction()
+						.remove(fragment)
+						.commit();
+			}
+		});
+
+		findViewById(R.id.addRight).setOnClickListener(v -> {
+			Fragment fragment = getSupportFragmentManager().findFragmentById(R.id.rightContainer);
+			if (fragment == null) {
+				fragment = new ExampleFragment();
+				getSupportFragmentManager()
+						.beginTransaction()
+						.add(R.id.rightContainer, fragment)
+						.commit();
+			}
+		});
+		findViewById(R.id.removeRight).setOnClickListener(v -> {
+			Fragment fragment = getSupportFragmentManager().findFragmentById(R.id.rightContainer);
+			if (fragment != null) {
+				getSupportFragmentManager()
+						.beginTransaction()
+						.remove(fragment)
+						.commit();
+			}
+		});
+
 		this.model = new PlayerModel<>(this, new InMemoryPlaybackSettings(), new LocalPlaybackFactory<>());
 	}
 
