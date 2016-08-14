edda811a29f29fde74b043cb66e4461f004e2546
Yaroslav Zozulia <zyvpeople@gmail.com>
2016-08-14 22:29:37 +0300

    created base classes for widget

---
+++ app/src/main/java/com/develop/zuzik/audioplayerexample/presentation/appwidget_provider/PlayerAppWidgetProvider.java
@@ -0,0 +1,11 @@
+package com.develop.zuzik.audioplayerexample.presentation.appwidget_provider;
+
+import android.appwidget.AppWidgetProvider;
+
+/**
+ * User: zuzik
+ * Date: 8/14/16
+ */
+
+public class PlayerAppWidgetProvider extends AppWidgetProvider {
+}
